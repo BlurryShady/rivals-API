26959291d06ecc1bf7355a4bfd7ba44acd6a61ac
BlurryShady <blurryshady@hotmail.com>
2025-12-15 21:01:37 +0300

    Added redirection to main page in deployment

--- marvel_rivals/urls.py
+++ marvel_rivals/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from django.shortcuts import redirect
 from marvel_rivals.health import health_view
 
+
+def root_view(_request):
+    return redirect("/api/health/")
+
+
 urlpatterns = [
+    path("", root_view),
     path('admin/', admin.site.urls),
     path('api/health/', health_view),
     path('api/', include('heroes.urls')),
